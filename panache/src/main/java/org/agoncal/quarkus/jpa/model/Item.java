c462b9a509ea50ecf90f82658f747d0f72eb3db0
Antonio Goncalves <antonio.goncalves@gmail.com>
2021-04-11 21:19:28 +0200

    Adding tests

--- panache/src/main/java/org/agoncal/quarkus/jpa/model/Item.java
+++ panache/src/main/java/org/agoncal/quarkus/jpa/model/Item.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 /**
@@ -23,6 +24,7 @@ public class Item extends PanacheEntity {
   // =             Attributes             =
   // ======================================
 
+  @NotNull
   public String title;
 
   @Column(length = 3000)
