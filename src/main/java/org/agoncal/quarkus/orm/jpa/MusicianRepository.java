91764587b68d7941687e4d8d02d2369e55040fbb
Antonio Goncalves <antonio.goncalves@gmail.com>
2021-05-20 18:09:07 +0200

    removing old code

--- src/main/java/org/agoncal/quarkus/orm/jpa/MusicianRepository.java
+++
@@ -1,9 +0,0 @@
-package org.agoncal.quarkus.orm.jpa;
-
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-
-import javax.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class MusicianRepository implements PanacheRepository<Musician> {
-}
--- src/test/java/org/agoncal/quarkus/orm/rest/NativeItemResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.agoncal.quarkus.orm.rest;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeItemResourceIT extends ItemResourceTest {
-
-    // Execute the same tests but in native mode.
-}
