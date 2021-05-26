4daf5b1a9f3b32291432520cc8cd9375f3cc2b60
Antonio Goncalves <antonio.goncalves@gmail.com>
2021-05-26 14:14:25 +0200

    Excluding JDBC drivers

--- vintage-store/src/main/java/org/agoncal/quarkus/panache/repository/CustomerRepository.java
+++ vintage-store/src/main/java/org/agoncal/quarkus/panache/repository/CustomerRepository.java
@@ -1,10 +1,10 @@
 package org.agoncal.quarkus.panache.repository;
 
-import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import org.agoncal.quarkus.jpa.Customer;
 
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class CustomerRepository implements PanacheRepositoryBase<Customer, Long> {
+public class CustomerRepository implements PanacheRepository<Customer> {
 }
